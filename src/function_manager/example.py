7b43b6d02b3de5953bcca583c7e6348ec5961423
lzjzx1122 <122567142@qq.com>
2021-03-26 15:53:05 +0800

    Update example.py

--- src/function_manager/example.py
+++ src/function_manager/example.py
@@ -3,6 +3,5 @@ from function_manager import FunctionManager
 
 manager = FunctionManager("functions")
 for _ in range(10):
-    retval = manager.run("utility", request_id, 1, {"runtime": 1, "output_size":10})
-    # run(function_name, request_id, runtime, input:{"file1": {"type": "MEM"/"DB", "value": path/doc}, "file2":... })
+    # manager.run(function_name, request_id, runtime, input:{"file1": {"type": "MEM"/"DB", "value": path/doc}, "file2":... }, output:{"file1": size, "file2": ...})
     # return {"file1": {"type": "MEM"/"DB", "value": path/doc}, "file2":...}
