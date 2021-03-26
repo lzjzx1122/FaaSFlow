ec6b3c6c94bad6506dc2ef17f7fc362bf0988e81
lzjzx1122 <122567142@qq.com>
2021-03-26 15:46:36 +0800

    Update example.py

--- src/function_manager/example.py
+++ src/function_manager/example.py
@@ -4,4 +4,6 @@ from function_manager import FunctionManager
 manager = FunctionManager("functions")
 for _ in range(10):
     request_id = uuid.uuid4().hex
-    manager.run("utility", request_id, {"runtime": 1, "output_size":10})
+    manager.run("utility", request_id, 1, {"runtime": 1, "output_size":10})
+    # run(function_name, request_id, runtime, input:{"file1": [MEM, path] / [DB, doc_name], "file2":... })
+    # return {"file1": [MEM, path] / [DB, doc_name], "file2":...}
