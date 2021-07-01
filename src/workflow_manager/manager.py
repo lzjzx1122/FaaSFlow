998fd93c70d287d0a3ceaf03230850a2453d2174
Jiagan Cheng <chnegjiagan@gmail.com>
2021-07-01 21:20:48 +0800

    change to workflow manager

--- src/workflow_manager/manager.py
+++ src/workflow_manager/manager.py
@@ -1,94 +1,48 @@
 from gevent import monkey
-
 monkey.patch_all()
-import gevent
-import repository
-import function_proxy
-import sys
+
 import time
+import sys
+import repository
+import gevent
 
 sys.path.append('../function_manager')
 from function_manager import FunctionManager
 
+class FakeFunc:
+    def __init__(self, req_id, func):
+        self.req_id = req_id
+        self.func = func
 
-class ConditionParser:
-    def __init__(self, request_id):
-        self.request_id = request_id
+    def __getattr__(self, name):
+        return repository.get_value(self.req_id, self.func, name)
 
-    def calculate(self, a, b, op):
-        if op == '+':
-            return a + b
-        elif op == '-':
-            return a - b
-        elif op == '*':
-            return a * b
-        else:
-            return a / b
 
-    def get_expr_value(self, expr):
-        operator = ['+', '-', '*', '/']
-        current_index = 0
-        num_stack = list()
-        op_stack = list()
-        while current_index < len(expr):
-            pre_index = current_index
-            while current_index < len(expr) and expr[current_index] not in operator:
-                current_index = current_index + 1
-            key = expr[pre_index, current_index].split('.')
-            function = key[0]
-            parameter = key[1]
-            value = repository.get_value(self.request_id, function, parameter)
-            num_stack.append(value)
-            if current_index == len(expr):
-                while len(op_stack) != 0:
-                    a = num_stack.pop()
-                    b = num_stack.pop()
-                    op = op_stack.pop()
-                    num_stack.append(self.calculate(a, b, op))
-                break
-            elif len(op_stack) == 0:
-                num_stack.append(value)
-                op_stack.append(expr[current_index])
-            else:
-                op = expr[current_index]
-                if op == '+' or op == '-':
-                    a = num_stack.pop()
-                    b = num_stack.pop()
-                    op2 = op_stack.pop()
-                    num_stack.append(self.calculate(a, b, op2))
-                    op_stack.append(op)
-                else:
-                    if op_stack[-1] == '*' or op_stack[-1] == '/':
-                        a = num_stack.pop()
-                        b = num_stack.pop()
-                        op2 = op_stack.pop()
-                        num_stack.append(self.calculate(a, b, op2))
-                        op_stack.append(op)
-                    else:
-                        op_stack.append(op)
-        return 0
+def cond_exec(req_id, cond):
+    if cond.startswith('default'):
+        return True
 
-    def parse_condition(self, condition):
-        if condition.startswith('default'):
-            return True
-        condition = condition.replace(' ', '')
-        if '<' in condition:
-            place = condition.find('<')
-            left_value = self.get_expr_value(condition[:place])
-            right_value = self.get_expr_value(condition[place + 1:])
-            return left_value < right_value
-        elif '>' in condition:
-            place = condition.find('>')
-            left_value = self.get_expr_value(condition[:place])
-            right_value = self.get_expr_value(condition[place + 1:])
-            return left_value > right_value
-        elif '==' in condition:
-            place = condition.find('==')
-            left_value = self.get_expr_value(condition[:place])
-            right_value = self.get_expr_value(condition[place + 2:])
-            return left_value == right_value
-        else:
-            return False
+    values = {}
+    while True:
+        try:
+            res = eval(cond)
+            break
+        except NameError as e:
+            name = str(e).split("'")[1]
+            values[name] = FakeFunc(req_id, name)
+
+    return res
+
+
+class WorkflowState:
+    def __init__(self, workflow, grouping, host, req_id):
+        self.workflow = workflow
+        self.grouping = grouping
+        self.parent_executed = {}
+        for func, node in grouping.items():
+            if node == host:
+                self.parent_executed[func] = 0
+        self.req_id = req_id
 
 
 class WorkflowManager:
@@ -97,15 +51,16 @@ class WorkflowManager:
         self.function_info = dict()
         self.executed = set()
         repository.create_result_db()
-        self.function_manager = FunctionManager("../function_manager/functions")
+        self.function_manager = FunctionManager(
+            "../function_manager/functions")
         self.mode = mode
         self.parent_executed = dict()
-        self.condition_parser = ConditionParser(request_id)
 
     def get_function_info(self, function_name):
         if function_name not in self.function_info:
             print('function_name: ', function_name)
-            self.function_info[function_name] = repository.get_function_info(function_name, self.mode)
+            self.function_info[function_name] = repository.get_function_info(
+                function_name, self.mode)
         return self.function_info[function_name]
 
     def run_function(self, function_name):
@@ -117,8 +72,9 @@ class WorkflowManager:
             jobs = []
             for index in range(len(function_info['next'])):
                 condition = function_info['conditions'][index]
-                if self.condition_parser.parse_condition(condition):
-                    jobs.append(gevent.spawn(self.run_function, function_info['next'][index]))
+                if cond_exec(self.request_id, condition):
+                    jobs.append(gevent.spawn(self.run_function,
+                                function_info['next'][index]))
                     break
             gevent.joinall(jobs)
             return
