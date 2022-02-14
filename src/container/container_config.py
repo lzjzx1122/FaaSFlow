3eb12a5758b537e10eea791a9c9d4ee068d4ebda
lzjzx1122 <122567142@qq.com>
2022-02-14 22:09:01 +0800

    Update container_config.py

--- src/container/container_config.py
+++ src/container/container_config.py
@@ -1,4 +1,4 @@
 COUCHDB_URL = 'http://openwhisk:openwhisk@172.17.0.1:5984/'
-REDIS_HOST = '172.17.0.1'
+REDIS_HOST = '172.17.0.1' # it serves to communicate with the host-side components from the docker container, and it should be 172.17.0.1
 REDIS_PORT = 6379
 REDIS_DB = 0
